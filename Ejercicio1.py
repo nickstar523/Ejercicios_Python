e54341773f66cab137065b965b66579401f42e02
Nicolas Sarmiento <nicolasarmiento1997@gmail.com>
2024-07-29 17:40:45 -0500

    Agrego esto al repo

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+mensaje= "hola, estoy aprendiendo python"
+nombre= "Nicolás"
+formacion= "ADSO"
+edad= 19
+estatura= 1.74
+peso= 62
+#forma de imprimir 1
+print(mensaje,"Mi nombre es:",nombre,"soy de la formación:",formacion,"tengo:",edad,"años","mido:", estatura,"y peso:",peso,"kg")
+#forma de imprimir 2
+print(f"{mensaje}. Mi nombre es: {nombre}, soy de la formacion: {formacion}, Tengo: {edad} años, mido: {estatura}, y peso: {peso} kg")
